ef65a9c898f3166b34d0ddc79457d7cea599ecb2
sebastianmina <69209782+sebastianmina@users.noreply.github.com>
2021-08-18 17:16:27 -0500

    TALLER 1

--- src/main/java/edu/escuelaing/arep/sparkprimer/App.java
+++ src/main/java/edu/escuelaing/arep/sparkprimer/App.java
@@ -1,5 +1,7 @@
 package edu.escuelaing.arep.sparkprimer;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       get("/hello", (req, res) -> "Hello World");
     }
 }
